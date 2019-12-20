c507dc3c11c38e7e5080e384db19b45b00553f68
Shalia Wynn <13423248+swynn730@users.noreply.github.com>
2019-12-20 02:36:46 -0600

    Finished part 2 of day 6.

--- day06/day06.py
+++ day06/day06.py
@@ -1,4 +1,5 @@
 import copy
+import itertools
 # Part 1
 # Answer: 245089
 # Needed some help with part 1, because even though the original algorithm I came up with worked on the test data it didn't work with the real data.
@@ -12,6 +13,9 @@ with open("input.txt") as f_handle:
 	# :TEST DATA FOR PART 1:
 	# f_content = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
 	# The answer for the total number of direct and indirect orbits is 42.
+	# :TEST DATA FOR PART 2:
+	# f_content = ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"]
+	# The answer for the minimum number of orbital transfers is 4.
 	for orbit in f_content:
 		# The parent is the object being orbited.
 		# The child is the object orbiting the parent.
@@ -40,7 +44,8 @@ for orbit_map_key in orbit_map_keys:
 		# F -> E -> D -> C -> B -> COM
 		# We have to do [0] because the new val data will be in a list but there will only ever be 1 element for the val in the original orbit map.
 			return traverse(op, op[val][0]) + [val]
-	# Update the copy with the complete orbit route.
+	# Clear out existing content and update the copy with the complete orbit route. This way we don't get a duplicate entry at the very beginning of the list.
+	orbit_map_copy[orbit_map_key] = []
 	for orbit_map_value in orbit_map_values:
 		orbit_map_copy[orbit_map_key] += traverse(orbit_map, orbit_map_value)
 
@@ -48,10 +53,18 @@ for orbit_map_key in orbit_map_keys:
 total_direct_and_indirect_orbits = 0
 orbit_map_copy_values = orbit_map_copy.values()
 for orbit_map_copy_value in orbit_map_copy_values:
-	# Some duplicates get in there some how so let's clean that up.
-	total_direct_and_indirect_orbits += len(set(orbit_map_copy_value))
+	total_direct_and_indirect_orbits += len(orbit_map_copy_value)
 
 print(total_direct_and_indirect_orbits)
 
 # Part 2
-# Answer:
+# Answer: 511
+# :THIS CODE IS FOR PART 2 ONLY:
+san_orbits = orbit_map_copy["SAN"]
+you_orbits = orbit_map_copy["YOU"]
+# Substract the common objects from each route to get the unique objects. 
+# The summation or in this case union of the unique objects together represents the minimum number of orbital transfers
+# required to move from the object YOU are orbiting to the object SAN is orbiting.
+# (Between the objects they are orbiting NOT between YOU and SAN).
+intersecting_orbits = set(san_orbits).intersection(set(you_orbits))
+print(len(set(san_orbits).difference(intersecting_orbits).union(set(you_orbits).difference(intersecting_orbits))))
